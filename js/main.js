d60a800ec5ab2d07742768d6a816d08b4bb85870
Finn Lawrence <finn.lawrence@gmail.com>
2016-05-11 20:50:54 +1200

    Improvements to UX

--- js/main.js
+++ js/main.js
@@ -7,8 +7,30 @@ $(document).ready(function() {
     $("#searchbar").submit(function() {
         event.preventDefault();
         var url = $('#search').val();
+        
+        if (! isValidURL(url)) {
+            url = "http://" + url;
+            
+            if (! isValidURL(url)) {
+                alert("Please enter a valid URL");
+                return false;
+            } else {
+                $('#search').val(url);
+            }
+        }
+        $('#search').blur();
         $('iframe').each(function() {
             $(this).attr("src", url);
         });
     });
 });
+
+
+
+function isValidURL(url) {
+     if(url.match(/^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/)) {
+         return true;
+     } else {
+         return false;
+     }
+}
