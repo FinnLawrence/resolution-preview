a5830a76f08fd8f9cb343911899b7c235a1a9446
Finn Lawrence <finn.lawrence@gmail.com>
2016-05-19 17:08:46 +1200

    Added analytics and parameters

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 ---
 
 $(document).ready(function() {
-    $("input").focus(function() { $(this).select(); } );
+    $('input').focus(function() { $(this).select(); } );
     
-    $("#searchbar").submit(function() {
+    $('#searchbar').submit(function(event) {
         event.preventDefault();
         var url = $('#search').val();
         
@@ -18,14 +18,32 @@ $(document).ready(function() {
                 $('#search').val(url);
             }
         }
+        setParameters(url);
         $('#search').blur();
         $('iframe').each(function() {
             $(this).attr("src", url);
         });
     });
+
+    parseParameters();
 });
 
+function parseParameters() {
+    var pageURL = window.location.href;
+    var site = url('?url', pageURL);
+    
+    if(site) {
+        $('#search').val(site);
+        $('#searchbar').submit();
+    }
+}
 
+function setParameters(pageURL) {
+    var domain = url('hostname', pageURL);
+    var path = url('path', pageURL);
+    pageURL = url('hostname', window.location.href) + "?url=" + domain + path;
+    window.history.pushState(null, null, pageURL);
+}
 
 function isValidURL(url) {
      if(url.match(/^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/)) {
